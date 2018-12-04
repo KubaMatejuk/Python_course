db5187888f7fc69e3f47d9a9cf0da8d84a0507d3
Kuba <jmatejuk@gmail.com>
2018-12-04 11:03:49 +0100

    Zajecia 4

---
+++ Spotkanie4/bmi.py
@@ -0,0 +1,25 @@
+def bmi(masa, wzrost):
+    return round(masa/(wzrost*wzrost),2)
+
+def komentarz(bmi):
+    if bmi <16:
+        tekst = "wygłodzenie"
+    elif bmi <17:
+        tekst = "wychudzenie"
+    elif bmi < 18.5:
+        tekst = "niedowaga"
+    elif bmi < 25:
+        tekst = "wartość prawidłowa"
+    elif bmi < 30:
+        tekst = "nadwaga"
+    elif bmi < 35:
+        tekst = "I stopień otyłości"
+    elif bmi < 40:
+        tekst = "II stopień otyłości"
+    else:
+        tekst = "III stopień otyłości(otyłość skrajna)"
+    return tekst
+
+waga=87
+wzrost_w_metrach = 1.8
+print("Twoje BMI wynosi {0} i oznacza: {1}".format(bmi(waga,wzrost_w_metrach),komentarz(bmi(waga,wzrost_w_metrach))))
---
+++ Spotkanie4/osobowe.py
@@ -0,0 +1,47 @@
+import bmi
+class Osoba:
+
+    def __init__(self, imie, nazwisko, waga, wzrost):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.waga = waga
+        self.wzrost = wzrost
+
+    def __str__(self):
+        return str(self.__class__.__name__) + ": " + self.imie + " " + self.nazwisko
+
+    def bmi(self):
+        return bmi.bmi(self.waga, self.wzrost)
+
+class Pracownik(Osoba):
+
+    def __init__(self, imie, nazwisko, pensja, waga, wzrost):
+        Osoba.__init__(self, imie, nazwisko, waga, wzrost)
+        self.pensja = pensja
+
+    def wyplata(self):
+        return self.pensja * 1.2
+
+
+class Kierownik(Pracownik):
+    """
+    def __init__(self, imie, nazwisko, pensja):
+        Pracownik.__init__(self, imie, nazwisko, pensja)
+    """
+
+    def wyplata(self):
+        return super().wyplata() + 1200.0
+
+
+
+o = Osoba("Jan",  "Kowalski", 100, 2)
+print(o)
+
+p = Pracownik("Jan", "Nowak", 2250, 100, 2)
+
+print("{0} wypłata {1}".format(p, p.wyplata()))
+
+k = Kierownik("Anna", "Kowalska", 5000, 100,2)
+print(k.wyplata())
+print(k)
+print(k.bmi())
